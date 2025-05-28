7f51c2f627f992e74026f75af59d67c4885c020f
Stuart Espey <stuart.espey@mactrix.com>
2025-05-28 07:20:41 +0000

    test/storage/truenas: re-enable storage test

--- test/suites/storage.sh
+++ test/suites/storage.sh
@@ -777,7 +777,7 @@ test_storage() {
   rootMinKiB2="720000"
   rootMaxKiB2="850000"
 
-  if [ "$incus_backend" != "dir" ] && [ "$incus_backend" != "truenas" ]; then # truenas is currently limited to >= 1GiB sizes.
+  if [ "$incus_backend" != "dir" ] ; then
     incus launch testimage quota1
     rootOrigSizeKiB=$(incus exec quota1 -- df -P / | tail -n1 | awk '{print $2}')
     rootOrigMinSizeKiB=$((rootOrigSizeKiB-2000))
